7e25beda6f7d395aa321a7007331ecbf159c5807
github username <omidvatan94@gmail.com>
2019-03-08 14:24:17 -0500

    trying different API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DarkSkyApi from 'dark-sky-api'
+
 
 class App extends Component {
 
-  state = {
-    response: {}
-  }
 
   render() {
 
-    DarkSkyApi.apiKey = 'a10e2251ba5b1dc81cc591194eaa43b7';
-    DarkSkyApi.proxy = true;
-    DarkSkyApi.loadForecast()
-  .then(result => console.log(result));
+  
     return (
 
       <div className="App">
