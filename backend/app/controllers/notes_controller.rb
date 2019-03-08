bf9ed3f8bdb79b7e5960c6b4972ad37c2b68fbb8
github username <omidvatan94@gmail.com>
2019-03-08 16:49:00 -0500

    more work on backend

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -1,2 +1,31 @@
 class NotesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  def index
+   @notes = Note.all
+   render json: @notes
+  end
+
+  def create
+    @note = Note.new(title: params[:title], body: params[:body])
+    if @note.save
+      render json: @note
+    else
+      render json: { error: 'WRONG'}, status: 422
+    end
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render json: @note
+  end
+
+  # def update
+  #   @note = Note.find(params[:id])
+  #   @note.update(title: params[:title], body: params[:body])
+  #   render json: @note
+  # end
+
+
+
 end
