899ccfd732970d338ad102e53fdc0d23368226a5
github username <omidvatan94@gmail.com>
2019-03-08 14:12:30 -0500

    request working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DarkSkyApi from 'dark-sky-api'
 
 class App extends Component {
 
-  componentDidMount(){
-    fetch("https://api.darksky.net/forecast/a10e2251ba5b1dc81cc591194eaa43b7/37.8267,-122.4233", {
-      mode: "no-cors"
-    })
-    .then(res => res.json())
-    .then(console.log)
+  state = {
+    response: {}
   }
+
   render() {
+
+    DarkSkyApi.apiKey = 'a10e2251ba5b1dc81cc591194eaa43b7';
+    DarkSkyApi.proxy = true;
+    DarkSkyApi.loadForecast()
+  .then(result => console.log(result));
     return (
+
       <div className="App">
         <header className="App-header">
 
