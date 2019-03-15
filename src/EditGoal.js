81198a93f90f07524b4e5e868e51277e9b0ff627
github username <omidvatan94@gmail.com>
2019-03-15 14:47:36 +0100

    more components

---
+++ src/EditGoal.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class EditGoal extends PureComponent {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default EditGoal;
